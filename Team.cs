ea6f8ff95856161218dbf2e6b4bd161e5a4313d3
Ion Chiriac <ion.chiriac@amdaris.com>
2019-02-08 13:48:41 +0200

    Added interfaces Game Classis and some logics

--- Team.cs
+++ Team.cs
@@ -6,28 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Team
-    {
-        // Team
-        List<Player> player = new List<Player>() ;
-
-        public void AddPlayer(String name)
-        {
-            player.Add(new Player() { Name = name });
-   
-        }
-        public void GetPlayers()
-            {
-            if (player.Count > 0)
-                foreach (Player p in player)
-                {
-                    Console.WriteLine($"Player Name : { p.Name}");
-                }
-            else Console.WriteLine("Is no any plaers in the List !!!");
-            }
-    }
-
-    class Player
+ class Player
     {
        public Player() {
             Name = "NoName";
@@ -36,15 +15,109 @@ namespace ConsoleApp1
 
 
     }
-    // bsket , fotball , volei
 
+    class Team
+    {
+        // Team
+        List<Player> player = new List<Player>() ;
+        public String TeamName { get; set; }
+
+        public void AddPlayer(Player p)
+        {
+            player.Add(p );
+   
+        }
+        public void GetPlayers()
+        {
+            if (player.Count > 0)
+                foreach (Player p in player)
+                {
+                    Console.WriteLine($"Player Name : { p.Name}");
+                }
+            else Console.WriteLine("Is no any plaers in the List !!!");
+        }
+    }
+    
     interface IGame
     {
+       
+
          String GetScore();
-        String GetTeams();
+         String GetTeams();
+
+         void AddTeam(Team team);
 
 
     }
+
+   
+    // bsket , fotball , volei
+
+    class Basket :  IGame
+    {
+        private Team[] teams = new Team[2];
+        private int id = 0;
+        private int team1Score = 0;
+        private int team2Score = 0;
+        public  void AddTeam(Team team)
+        {
+            if (id < 2)
+            {
+                teams[id] = team;
+                id++;
+            }
+            else
+            {
+                Console.WriteLine("In game can be only 2 Teams !");
+            }
+        }
+
+        public  string GetScore()
+        {
+
+            return $"Score is : | {teams[0].TeamName} : {team1Score} vs {teams[1].TeamName} : {team2Score}  |";
+        }
+
+        public  string GetTeams()
+        {
+            return $" {teams[0].TeamName} vs {teams[1].TeamName}";
+        }
+    }
+
+
+    class fotball : IGame
+    {
+        private int id = 0;
+        private int team1Score = 0;
+        private int team2Score = 0;
+        private Team[] teams = new Team[2];
+        public void AddTeam(Team team)
+        {
+            if (id < 2)
+            {
+                teams[id] = team;
+                id++;
+            }
+            else
+            {
+                Console.WriteLine("In game can be only 2 Teams !");
+            }
+        }
+
+        public string GetScore()
+        {
+
+            return $"Score is : | {teams[0].TeamName} : {team1Score} vs {teams[1].TeamName} : {team2Score}  |";
+        }
+
+        public string GetTeams()
+        {
+            return $" {teams[0].TeamName} vs {teams[1].TeamName}";
+        }
+    }
+
+
+
 
 
 
