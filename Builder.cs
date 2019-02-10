7c00e82214cc2f1118de2ef98bdec1de9cf0aab8
Ion <chiriac.ion97@outlook.com>
2019-02-11 01:37:46 +0200

    Add New Experiment Builder

---
+++ Builder.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOopC_
+{
+    public class Order
+    {
+
+      public  class Builder
+        {
+            private String bred;
+            private String met;
+            private String chees;
+
+
+
+            public Order build()
+            {
+                return new Order(this);
+            }
+
+            public Builder Bread(String bread)
+            {
+                this.bred = bread;
+                return this;
+            }
+            public Builder Met(String met)
+            {
+                this.met = met;
+                return this;
+            }
+            public Builder Chees(String chees)
+            {
+                this.chees = chees;
+                return this;
+            }
+
+            public String GetMet()
+            {
+                return this.met;
+            }
+            public string GetChees()
+            {
+                return this.chees;
+            }
+            public String GetBread()
+            {
+                return this.bred;
+            }
+        }
+
+        private Order(Builder builder)
+        {
+            this.bred = builder.GetBread();
+            this.chees = builder.GetChees();
+            this.met = builder.GetMet();
+        }
+
+        public String met { get; }
+        public String bred { get; }
+        public String chees { get; }
+
+
+    }
+    //------experiment--------------------------------------------------------------------------------------------------------------
+
+    class MyClass
+    {
+        private string aa;
+        private string bb;
+        private string cc;
+
+        public MyClass SetA(string a)
+        {
+            this.aa = a;
+            return this;
+        }
+        public MyClass SetB(string b)
+        {
+            this.bb = b;
+            return this;
+
+        }
+        public MyClass SetC(string c)
+        {
+            this.cc = c;
+            return this;
+
+        }
+
+        public string GetAll()
+        {
+            return $"{aa} {bb} {cc}";
+        }
+
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -4,60 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace GameOopC_
 {
-  class Program
-  {
-      static void Main(string[] args)
-      {
-          //  SOLID :)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  SOLID :)
 
-          Basket basket = new Basket();
-          Fotball fotball = new Fotball();
-          Team team = new Team() { TeamName = "TI" };
-          Player player = new Player() { Name = "Ion" };
+            Basket basket = new Basket();
+            Fotball fotball = new Fotball();
+            Team team = new Team() { TeamName = "TI" };
+            Player player = new Player() { Name = "Ion" };
 
-          AddPlayerToTeam(player, team);
-          AddTeamToGame(team, basket);
+            AddPlayerToTeam(player, team);
+            AddTeamToGame(team, basket);
 
-          player = new Player() { Name = "Nicu" };
-          team = new Team() { TeamName = "FAF" };
+            player = new Player() { Name = "Nicu" };
+            team = new Team() { TeamName = "FAF" };
 
-          AddPlayerToTeam(player, team);
-          AddTeamToGame(team, basket);
-
-
-          GetTeams(basket);
-          Console.WriteLine("\n -xxx------------------------------------------------------- \n");
-          ShowGamesAndTeams(new List<IGameShow>() { basket, fotball, new SomeGame() });
+            AddPlayerToTeam(player, team);
+            AddTeamToGame(team, basket);
 
 
-          Console.ReadLine();
+            GetTeams(basket);
+            Console.WriteLine("\n -xxx------------------------------------------------------- \n");
+            ShowGamesAndTeams(new List<IGameShow>() { basket, fotball, new SomeGame() });
 
-      }
-      public static void AddTeamToGame(ITeam team, IGameAdd game)
-      {
-          game.AddTeam(team);
-      }
-      public static void AddPlayerToTeam(IPlayer player, ITeam team)
-      {
-          team.AddPlayer(player);
-      }
-      public static void GetTeams(IGameShow game)
-      {
-          Console.WriteLine(game.GetTeams());
-          game.ShowPlayersAndTeams();
-      }
 
-      public static void ShowGamesAndTeams(List<IGameShow> games)
-      {
-          //Liskov efect
-          foreach (IGameShow g in games)
-          {
-              g.ShowPlayersAndTeams();
-              Console.WriteLine("\n -------------------------------------------------------- \n");
-          }
-      }
+//---experiment-------------------------------------------------------------------------------------------------------
+            Order.Builder builder = new Order.Builder();
+            builder.Bread("Wheat").Chees("America").Met("Ham");
 
-  }
+            Order order = builder.build();
+
+            Console.WriteLine($"{order.bred} , {order.met} , {order.chees} .");
+            //----experiment-----------------------------------------------------------------------------------------------------------------------------
+
+            MyClass myClass = new MyClass();
+
+            myClass.SetA("aaaa").SetB("bbbb").SetC("cccc");
+            Console.WriteLine(myClass.GetAll());
+//-----------------------------------------------------------------------------------------------------------------------
+            Console.ReadLine();
+
+        }
+        public static void AddTeamToGame(ITeam team, IGameAdd game)
+        {
+            game.AddTeam(team);
+        }
+        public static void AddPlayerToTeam(IPlayer player, ITeam team)
+        {
+            team.AddPlayer(player);
+        }
+        public static void GetTeams(IGameShow game)
+        {
+            Console.WriteLine(game.GetTeams());
+            game.ShowPlayersAndTeams();
+        }
+
+        public static void ShowGamesAndTeams(List<IGameShow> games)
+        {
+            //Liskov efect
+            foreach (IGameShow g in games)
+            {
+                g.ShowPlayersAndTeams();
+                Console.WriteLine("\n -------------------------------------------------------- \n");
+            }
+        }
+
+    }
 }
